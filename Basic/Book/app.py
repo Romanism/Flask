cff6c55755488bdda42523f0581ea9a3fb0b0bb0
JISUNGIN <jsi920629@gmail.com>
2019-08-17 13:15:13 +0900

    Fix: 로그인 기능 구현중..(80%)

--- Basic/Book/app.py
+++ Basic/Book/app.py
@@ -1,6 +1,11 @@
-from flask import Flask, request, jsonify, current_app
+import jwt
+import bcrypt
+
+from flask import Flask, request, jsonify, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
+from datetime import datetime, timedelta
+from functools import wraps
 
 class CustomJSONEncoder(JSONEncoder):
     """ 
@@ -122,6 +127,46 @@ def get_timeline(user_id):
     } for tweet in timeline]
 
 
+def get_user_id_and_password(email):
+    row = current_app.database.execute(text("""
+        SELECT
+            id,
+            hashed_password
+        FROM users
+        WHERE email = :email
+    """), {"email" : email}).fetchone()
+
+    return {
+        "id" : row["id"],
+        "hashed_password" : row["hashed_password"]
+    } if row else None
+
+
+#########################################################
+#                      Decorators                       #
+#########################################################
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get("Authorization")
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app, config["JWT_SECRET_KEY"], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+            
+            if payload is None: 
+                return Response(status=401)
+
+            user_id = payload["user_id"]
+            g.user_id = user_id
+            g.user = get_user(user_id) if user_id else None
+        else:
+            return Response(status=401)
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 def create_app(test_config = None):
     """
     - Flask가 create_app이라는 이름의 함수를 자동으로 팩토리 함수로 인식해서 해당 함수를 통해서 Flask 실행
@@ -148,6 +193,28 @@ def create_app(test_config = None):
         new_user = get_user(new_user_id)
 
         return jsonify(new_user)
+
+
+    @app.route("/login", method=["POST"])
+    def login():
+        credential = request.json
+        email = credential["email"]
+        password = credential["password"]
+        user_credential = get_user_id_and_password(email)
+
+        if user_credential and bcrypt.checkpw(password.encode("UTF-8"), user_credential["hashed_password"].encode("UTF-8")):
+            user_id = user_credential["id"]
+            payload = {
+                "user_id" : user_id,
+                "exp" : datetime.utcnow() + timedelta(seconds = 60 * 60 *  24)
+            }
+            token = jwt.encode(payload, app.config["JWT_SECRET_KEY"], "HS256")
+        
+            return jsonify({
+                "access_token": token.decode("UTF-8")
+            })
+        else:
+            return "no", 401
 
 
     @app.route("/tweet", methods=["POST"])
