e3c981ed8a0e10b3cbec77165b62aa42fad86060
JI <jsi920629@gmail.com>
2019-08-26 17:29:37 +0900

    Add: Blog tutorial - Templates

--- Blog/flaskblog.py
+++ Blog/flaskblog.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/home')
+posts = [
+    {
+        "author": "JI",
+        "title" : "Blog Post 1",
+        "content" : "First Post Content",
+        "date_posted": "August 26, 2019"
+    },
+    {
+        "author": "JI",
+        "title" : "Blog Post 2",
+        "content" : "Second Post Content",
+        "date_posted": "August 26, 2019"
+    },
+]
+
+
+@app.route("/home")
 def home():
-    return "<h1>Home Page!</h1>"
+    """
+    posts의 데이터를 home.html에 전달해 렌더링
+    """
+    return render_template("home.html", posts=posts)
+
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    """
+    about.html에 title을 건네줌
+    """
+    return render_template("about.html", title="about")
